dd6d69336a53e4fc00ff2b7bfb5d62163421583a
jangwanjung <tjdrj530@naver.com>
2025-01-29 01:49:01 +0900

    d

--- src/main/java/project/testspeed/wqdwq/Controller.java
+++ src/main/java/project/testspeed/wqdwq/Controller.java
@@ -1,9 +1,13 @@
-import org.springframework.web.bind.annotation.PostMapping;
+package project.testspeed.wqdwq;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controller {
 
     @PostMapping("/test")
-    public void main(){
+    public void main1(){
         long startTime = System.currentTimeMillis();
         for(int i =0 ;i<10000000 ; i++){
             System.out.println(i);
