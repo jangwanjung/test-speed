a8d5d750bbb06362c67c7d6e13584e6f67147f3e
jangwanjung <tjdrj530@naver.com>
2025-01-29 02:07:57 +0900

    d

--- src/main/java/project/testspeed/wqdwq/Controller.java
+++ src/main/java/project/testspeed/wqdwq/Controller.java
@@ -9,7 +9,7 @@ public class Controller {
     @PostMapping("/test")
     public long  main1(){
         long startTime = System.currentTimeMillis();
-        for(int i =0 ;i<10000000 ; i++){
+        for(int i =0 ;i<1000000 ; i++){
             System.out.println(i);
         }
         long endTime = System.currentTimeMillis();
